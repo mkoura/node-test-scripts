9d69718c2e5b62324f00e66e440932efd58f942c
Artur Wieczorek <artur.wieczorek@iohk.io>
2020-06-10 10:46:25 +0200

    Add pool related functions to common

--- e2e-automation-scripts/common.sh
+++ e2e-automation-scripts/common.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 export CARDANO_NODE_SOCKET_PATH=example/node-pool1/node.sock
+export genesis_filepath=example/genesis.json
 export testnet_magic=42
 export protocol_params_file="${PWD}/example/protocol-params.json"
 export counter_file=example/counter
+export pool_counter_file=example/pool_counter
 export root_addresses_dir=example/addresses
 export pool_1_dir=example/node-pool1
 export signing_key=${root_addresses_dir}/user1.skey
@@ -14,6 +16,11 @@ export slotLength=1
 if [ -f "${counter_file}" ]; then
     counter_value=$(cat "${counter_file}")
     export counter_value="${counter_value}"
+fi
+
+if [ -f "${pool_counter_file}" ]; then
+    pool_counter_value=$(cat "${pool_counter_file}")
+    export pool_counter_value="${pool_counter_value}"
 fi
 
 check_counter_file () {
@@ -28,6 +35,21 @@ check_counter_file () {
         touch "${counter_file}"
         echo 2 > "${counter_file}"
         export counter_value=2
+    fi
+}
+
+check_pool_counter_file () {
+    if [ -f "${pool_counter_file}" ]; then
+        echo "INFO: Pool counter file exist - read the counter and increment by 1" > /dev/tty
+        pool_counter_value=$(cat "${pool_counter_file}")
+        export pool_counter_value=$((pool_counter_value+1))
+        echo ${pool_counter_value} > "${pool_counter_file}"
+        echo "INFO: Pool current run: ${pool_counter_value}" > /dev/tty
+    else
+        echo "INFO: Pool counter file does NOT exist - initializing it with value 2" > /dev/tty
+        touch "${pool_counter_file}"
+        echo 2 > "${pool_counter_file}"
+        export pool_counter_value=2
     fi
 }
 
@@ -56,6 +78,26 @@ get_current_tip () {
         exit 1
     fi
     echo ${current_tip}
+    exit 0
+}
+
+get_pool_deposit () {
+    local pool_deposit="$(cat "${protocol_params_file}" | jq '.poolDeposit')"
+    echo ${pool_deposit}
+    exit 0
+}
+
+get_slots_per_kes_period () {
+    local slots_per_kes_period="$(cat ${genesis_filepath} | grep KESPeriod |grep -o '[0-9]\+')"
+    echo ${slots_per_kes_period}
+    exit 0
+}
+
+calculate_kes_period () {
+    local current_tip=$(get_current_tip)
+    local slots_per_kes_period=$(get_slots_per_kes_period)
+    local kes_period="$((${current_tip} / ${slots_per_kes_period}))"
+    echo ${kes_period}
     exit 0
 }
 
@@ -93,7 +135,7 @@ wait_for_new_tip () {
     initialTip=$(get_current_tip)
     actualTip=$(get_current_tip)
     echo "INFO: initialSlotNo: ${initialTip}" > /dev/tty
-    
+
     while [ "${actualTip}" = "${initialTip}" ]; do
         sleep ${slotLength}
 	actualTip=$(get_current_tip)
