564d61cd8465b7c2145db1d6904fe4833f188e98
dorin100 <Bica123!>
2020-06-29 21:11:25 +0300

    added delegation and stake pool creation scripts + utils

--- python_scripts/e2e_scenarios/create_register_and_delegate_stake_address_using_certificate.py
+++ python_scripts/e2e_scenarios/create_register_and_delegate_stake_address_using_certificate.py
@@ -9,19 +9,19 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir_path)
 
-from e2e_scenarios.constants import USER1_ADDRESS, USER1_SKEY_FILE_PATH
+from e2e_scenarios.constants import USER1_ADDRESS, USER1_SKEY_FILE_PATH, POOL1_COLD_VKEY_FILE_PATH
 from e2e_scenarios.utils import create_payment_key_pair_and_address, calculate_tx_fee, calculate_tx_ttl, send_funds, \
     get_address_balance, wait_for_new_tip, assert_address_balance, create_stake_key_pair_and_address, \
     create_stake_addr_registration_cert, get_key_deposit, delegate_stake_address, get_stake_address_info, \
-    get_stake_distribution
+    get_stake_distribution, create_stake_addr_delegation_cert
 
 # Scenario
 # 1. Step1: create 1 new payment key pair and addresses (addr0.addr)
 # 2. Step2: create 1 new stake key pair and addresses (addr0_stake.addr)
 # 3. Step3: create 1 stake addresses registration certificate
-# 4. Step4: send some funds from user1 (the faucet) to addr0.addr
-# 5. Step5: submit the stake addresses registration certificate through a tx
-# 6. Step6: delegate the addr0_stake.addr to one stake pool id
+# 4. Step4: create 1 stake addresses delegation certificate
+# 5. Step5: send some funds from user1 (the faucet) to addr0.addr
+# 6. Step6: submit the 2 above created certificates through a tx
 # 7. Step7: check the delegation details for addr0_stake.addr
 
 print("Creating a new folder for the files created by the current test...")
@@ -42,7 +42,12 @@ print(f"====== Step3: create 1 stake addresses registration cert")
 stake_addr_reg_cert_file = create_stake_addr_registration_cert(tmp_directory_for_script_files, stake_addr_vkey_file, addr_name)
 print(f"Stake address registration certificate created - {stake_addr_reg_cert_file}")
 
-print(f"====== Step4: send some funds from user1 (the faucet) to addr0.addr")
+print(f"====== Step4: create 1 stake addresses delegation certificate")
+stake_addr_deleg_cert_file = create_stake_addr_delegation_cert(tmp_directory_for_script_files, stake_addr_vkey_file,
+                                                               POOL1_COLD_VKEY_FILE_PATH, addr_name)
+print(f"Stake address delegation certificate created - {stake_addr_deleg_cert_file}")
+
+print(f"====== Step5: send some funds from user1 (the faucet) to addr0.addr")
 key_deposit = get_key_deposit()
 tx_ttl = calculate_tx_ttl()
 tx_fee = calculate_tx_fee(1, 2, tx_ttl, signing_keys=[USER1_SKEY_FILE_PATH])
@@ -70,11 +75,11 @@ assert_address_balance(src_address, src_add_balance_init - tx_fee - transferred_
 print(f"Check that the balance for destination address was correctly updated")
 assert_address_balance(dst_addresses_list[0], dst_init_balance + transferred_amounts_list[0])
 
-print(f"====== Step5: submit the certificate through a tx")
+print(f"====== Step6: submit the 2 certificates through a tx")
 tx_ttl = calculate_tx_ttl()
 src_address = addr
 signing_keys_list = [addr_skey_file, stake_addr_skey_file]
-certificates_list = [stake_addr_reg_cert_file]
+certificates_list = [stake_addr_reg_cert_file, stake_addr_deleg_cert_file]
 
 print(f"signing_keys_list: {signing_keys_list}")
 print(f"certificates_list: {certificates_list}")
@@ -89,22 +94,6 @@ print(f"src_add_balance_init: {src_add_balance_init}")
 send_funds(src_address, tx_fee + key_deposit, tx_ttl,
            certificates=certificates_list,
            signing_keys=signing_keys_list)
-
-wait_for_new_tip()
-wait_for_new_tip()
-
-print(f"Check that the balance for source address was correctly updated")
-assert_address_balance(src_address, src_add_balance_init - tx_fee)
-
-print(f"====== Step6: delegate the addr0_stake.addr to one stake pool id")
-src_add_balance_init = get_address_balance(src_address)
-first_pool_id_in_stake_distribution = list(get_stake_distribution())[0]
-print(f"Stake Pool ID: {first_pool_id_in_stake_distribution}")
-
-tx_fee = calculate_tx_fee(1, 1, tx_ttl, signing_keys=signing_keys_list)
-aa = delegate_stake_address(stake_addr_skey_file, first_pool_id_in_stake_distribution, tx_fee)
-
-print(f"aa: {aa}")
 
 wait_for_new_tip()
 wait_for_new_tip()
---
+++ python_scripts/e2e_scenarios/create_register_stake_pool.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+#!/usr/bin/env python2
+
+import os, sys
+from pathlib import Path
+
+# TO DO: this could be moved out of the scripts (somehow..)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir_path)
+
+from e2e_scenarios.constants import USER1_ADDRESS, USER1_SKEY_FILE_PATH
+from e2e_scenarios.utils import create_payment_key_pair_and_address, calculate_tx_fee, calculate_tx_ttl, send_funds, \
+    get_address_balance, wait_for_new_tip, assert_address_balance, create_stake_key_pair_and_address, \
+    create_stake_addr_registration_cert, get_key_deposit, delegate_stake_address, get_stake_address_info, \
+    get_pool_deposit, gen_kes_key_pair, gen_vrf_key_pair, gen_cold_key_pair_and_counter, gen_node_operational_cert, \
+    get_actual_kes_period, gen_pool_registration_cert, create_stake_addr_delegation_cert, get_ledger_state, \
+    get_registered_stake_pools_ledger_state, get_stake_pool_id
+
+# Scenario
+# 1. Step1: create 1 new payment key pair and addresses (addr0.addr)
+# 2. Step2: create 1 new stake key pair and addresses (addr0_stake.addr)
+# 3. Step3: create 1 stake addresses registration certificate
+# 4. Step4: send some funds from user1 (the faucet) to addr0.addr
+# 5. Step5: create the KES key pair
+# 6. Step6: create the VRF key pair
+# 7. Step7: create the cold key pair and node operational certificate counter
+# 8. Step8: create the node operational certificate
+# 9. Step9: create the stake pool registration certificate
+# 10. Step10: crete the owner-delegation.cert in order to meet the pledge requirements
+# 11. Step11: submit all 4 certificates through a tx
+# 12. Step12: check that the pool was registered
+
+addr_name = "owner"
+node_name = "poolX"
+pool_pledge = 12345
+pool_cost = 123456789
+pool_margin = 0.123
+
+print("Creating a new folder for the files created by the current test...")
+tmp_directory_for_script_files = "tmp_" + sys.argv[0].split(".")[0]
+Path(tmp_directory_for_script_files).mkdir(parents=True, exist_ok=True)
+
+print(f"====== Step1: create 1 new payment key pair and addresses ({addr_name}.addr)")
+addr, addr_vkey_file, addr_skey_file = create_payment_key_pair_and_address(tmp_directory_for_script_files, addr_name)
+print(f"Address successfully created - {addr}; {addr_vkey_file}; {addr_skey_file}")
+
+print(f"====== Step2: create 1 new stake key pair and addresses ({addr_name}_stake.addr)")
+created_stake_addresses_dict = {}
+stake_addr, stake_addr_vkey_file, stake_addr_skey_file = create_stake_key_pair_and_address(tmp_directory_for_script_files, addr_name)
+print(f"Stake address successfully created - {stake_addr}; {stake_addr_vkey_file}; {stake_addr_skey_file}")
+
+print(f"====== Step3: create 1 stake addresses registration cert")
+stake_addr_reg_cert_file = create_stake_addr_registration_cert(tmp_directory_for_script_files, stake_addr_vkey_file, addr_name)
+print(f"Stake address registration certificate created - {stake_addr_reg_cert_file}")
+
+print(f"====== Step4: send some funds from user1 (the faucet) to {addr_name}.addr")
+key_deposit = get_key_deposit()
+pool_deposit = get_pool_deposit()
+tx_ttl = calculate_tx_ttl()
+tx_fee = calculate_tx_fee(1, 2, tx_ttl, signing_keys=[USER1_SKEY_FILE_PATH])
+
+src_address = USER1_ADDRESS
+dst_addresses_list = [addr]
+transferred_amounts_list = [int(4 * tx_fee + key_deposit + pool_deposit + pool_pledge)]
+signing_keys_list = [USER1_SKEY_FILE_PATH]
+
+src_add_balance_init = get_address_balance(src_address)
+dst_init_balance = get_address_balance(dst_addresses_list[0])
+
+print(f"Send {transferred_amounts_list} Lovelace from {src_address} to {dst_addresses_list}")
+send_funds(src_address, tx_fee, tx_ttl,
+           destinations_list=dst_addresses_list,
+           transferred_amounts=transferred_amounts_list,
+           signing_keys=signing_keys_list)
+
+wait_for_new_tip()
+wait_for_new_tip()
+
+print(f"Check that the balance for source address was correctly updated")
+assert_address_balance(src_address, src_add_balance_init - tx_fee - transferred_amounts_list[0])
+
+print(f"Check that the balance for destination address was correctly updated")
+assert_address_balance(dst_addresses_list[0], dst_init_balance + transferred_amounts_list[0])
+
+print(f"====== Step5: create the KES key pair")
+node_kes_vkey_file, node_kes_skey_file = gen_kes_key_pair(tmp_directory_for_script_files, node_name)
+print(f"KEY keys created - {node_kes_vkey_file}; {node_kes_skey_file}")
+
+print(f"====== Step6: create the VRF key pair")
+node_vrf_vkey_file, node_vrf_skey_file = gen_vrf_key_pair(tmp_directory_for_script_files, node_name)
+print(f"VRF keys created - {node_vrf_vkey_file}; {node_vrf_skey_file}")
+
+print(f"====== Step7: create the cold key pair and node operational certificate counter")
+node_cold_vkey_file, node_cold_skey_file, node_cold_counter_file = gen_cold_key_pair_and_counter(tmp_directory_for_script_files, node_name)
+print(f"Cold keys created and counter created - {node_cold_vkey_file}; {node_cold_skey_file}; {node_cold_counter_file}")
+
+print(f"====== Step8: create the node operational certificate")
+kes_period = get_actual_kes_period()
+node_opcert_file = gen_node_operational_cert(node_kes_vkey_file, node_cold_skey_file, node_cold_counter_file,
+                                             tmp_directory_for_script_files, node_name)
+print(f"Node operational certificate created - {node_opcert_file}")
+
+print(f"====== Step9: create the stake pool registration certificate")
+pool_reg_cert_file = gen_pool_registration_cert(pool_pledge, pool_cost, pool_margin, node_vrf_vkey_file,
+                                                node_cold_vkey_file, stake_addr_vkey_file,
+                                                tmp_directory_for_script_files, node_name)
+print(f"Stake pool registration certificate created - {pool_reg_cert_file}")
+
+print(f"====== Step10: crete the owner-delegation.cert in order to meet the pledge requirements")
+stake_addr_delegation_cert_file = create_stake_addr_delegation_cert(tmp_directory_for_script_files, stake_addr_vkey_file,
+                                                                    node_cold_vkey_file, addr_name)
+print(f"Stake pool owner-delegation certificate created - {stake_addr_delegation_cert_file}")
+
+print(f"====== Step11: submit all the certificates through a tx")
+src_address = addr
+certificates_list = [node_opcert_file, pool_reg_cert_file, stake_addr_reg_cert_file, stake_addr_delegation_cert_file]
+signing_keys_list = [addr_skey_file, stake_addr_skey_file, node_cold_skey_file]
+
+tx_ttl = calculate_tx_ttl()
+tx_fee = calculate_tx_fee(1, 1, tx_ttl, certificates=certificates_list, signing_keys=signing_keys_list)
+
+src_add_balance_init = get_address_balance(src_address)
+
+send_funds(src_address, tx_fee + key_deposit, tx_ttl,
+           certificates=certificates_list,
+           signing_keys=signing_keys_list)
+
+wait_for_new_tip()
+wait_for_new_tip()
+
+print(f"Check that the balance for source address was correctly updated")
+assert_address_balance(src_address, src_add_balance_init - tx_fee)
+
+print(f"====== Step12: check that the pool was registered")
+stake_pool_id = get_stake_pool_id(node_cold_vkey_file)
+if stake_pool_id not in list(get_registered_stake_pools_ledger_state().keys()):
+    print(f"ERROR: newly created stake pool id is not shown inside the available stake pools; "
+          f"\n\t- Pool ID: {stake_pool_id} vs Existing IDs: {list(get_registered_stake_pools_ledger_state().keys())}")
+    exit(2)
+
+on_chain_stake_pool_details = get_registered_stake_pools_ledger_state().get(stake_pool_id)
+
+print(f"on_chain_stake_pool_details: {on_chain_stake_pool_details}")
